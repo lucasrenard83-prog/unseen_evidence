f30af56f0e133c3c6da7cda708659bcd3322fc97
Martin Cassan <martin.cassan@gmail.com>
2025-12-02 19:45:24 +0100

    adding intro messages

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,6 +7,20 @@ class RoomsController < ApplicationController
     @rooms = @game.rooms.order(:position)
     @messages = @game.messages
     @message = Message.new
+
+    # Shows the description of the room when entering if no message
+    intro_msg = "Entering #{@room.name} "
+    if @room.messages.count == 0
+      intro_msg += @room.public_description
+    end
+
+    @intro = Message.create(
+      role: "assistant",
+      content: intro_msg,
+      room: @room,
+      game: @game,
+      persona: Persona.find_by(room: @room)
+      )
   end
 
 end
