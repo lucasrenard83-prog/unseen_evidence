01a35c4278f2757709d23cb62b81cab1bf18632b
Martin Cassan <martin.cassan@gmail.com>
2025-12-03 14:57:47 +0100

    items modified

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -40,110 +40,177 @@ class GamesController < ApplicationController
   def rooms_init
     rooms_data = [
       { name: "Hall",
-        public_description: "The imposing entrance hall is an old school hall.
+        position: 1,
+        open: true,
+        description: "The imposing entrance hall is an old school hall.
         A grand varnished staircase dominates the room, its steps worn by years of footsteps.
         Portraits line the walls. The central carpet bears faint marks as if someone had run,
         hesitated… or fled. The air here feels colder than in the rest of the house.
         The player can search but nothing is hidden",
+        AI_guildeline: "...",
+        item_found: false,
+        before_picture_url: "...",
+        after_picture_url: "..."
+      },
+      { name: "Library",
+        position: 2,
         open: true,
-        position: 1
+        description: "...",
+        AI_guildeline: "...",
+        item_found: false,
+        before_picture_url: "...",
+        after_picture_url: "..."
+      },
+      { name: "Greenhouse",
+        position: 3,
+        open: false,
+        description: "...",
+        AI_guildeline: "...",
+        item_found: false,
+        before_picture_url: "...",
+        after_picture_url: "..."
+      },
+      { name: "Cellar",
+        position: 4,
+        open: false,
+        description: "...",
+        AI_guildeline: "...",
+        item_found: false,
+        before_picture_url: "...",
+        after_picture_url: "..."
+      },
+      { name: "Study",
+        position: 5,
+        open: true,
+        description: "...",
+        AI_guildeline: "...",
+        item_found: false,
+        before_picture_url: "...",
+        after_picture_url: "..."
       },
       { name: "Kitchen",
-        public_description: "White tiles and gleaming surfaces—almost too clean.
-        On the table rests a fruit bowl.
-        The fridge hums, the only steady noise in a room
-        where even a misplaced spoon could be a potential clue.
-        A faint smell of cold smoke hangs in the air,
-        though nothing here seems to have been burned recently. ",
+        position: 6,
         open: true,
-        position: 2},
-      { name: "Library",
-        public_description: " A faint scent of old paper lingers in the air,
-        mixed with a discreet hint of wood polish. Shelves climb all the way to the ceiling,
-        packed with far too many books for anyone to truly read. A green-shaded lamp casts a
-        tired glow over an oak desk where a cup of tea sits cold, as if time itself had stopped.
-        The silence is heavy, almost expectant, as though the walls are waiting for someone to
-        finally uncover a secret kept far too long. ",
-        open: true,
-        position: 3},
-      { name: "Cellar",
-        public_description: "Darkness clings to the damp walls,
-        forming a heavy, confined atmosphere. The dirt floor shows irregular small prints.
-        Shelves filled with jars line the room under a single exposed bulb that flickers
-        with a nervous rhythm. A draft from nowhere cuts through the stale air,
-        almost as if something had moved here not long ago. ",
-        open: false,
-        position: 4},
+        description: "...",
+        AI_guildeline: "...",
+        item_found: false,
+        before_picture_url: "...",
+        after_picture_url: "..."
+      },
       { name: "Attic",
-        public_description: "The attic is wide and cluttered,
-        smelling of dry wood and forgotten memories. Beneath the sloping roof,
-        trunks and boxes pile up, some slightly open, revealing fragments of a
-        past no one has spoken of. Light seeps through loose tiles in thin dusty beams.
-        At the center lies a toppled chair and a circular mark on the floor—signs
-        of something that happened here… whatever it was. ",
-        open: false,
-        position: 5},
-      { name: "Bureau",
-        public_description: "Flooded with daylight, yet strangely cold.
-        The potted plants, luxuriant sea of greens. Rain taps rhythmically
-        against the glass panes, giving the impression of someone whispering just outside.
-        A round table is scattered with slightly damp papers and a pen frozen mid-stroke,
-        abandoned in a moment of abrupt interruption.",
-        open: false,
-        position: 6},
-      { name: "Greenhouse",
-        public_description: "Flooded with daylight, yet strangely cold.
-        The potted plants, luxuriant sea of greens. Rain taps rhythmically
-        against the glass panes, giving the impression of someone whispering just outside.
-        A round table is scattered with slightly damp papers and a pen frozen mid-stroke,
-        abandoned in a moment of abrupt interruption.",
-        open: false,
-        position: 7},
+        position: 7,
+        open: true,
+        description: "...",
+        AI_guildeline: "...",
+        item_found: false,
+        before_picture_url: "...",
+        after_picture_url: "..."
+      }
     ]
     rooms_data.each { |room| @game.rooms.create!(room)}
   end
 
   def personas_init
     persona_data = [
-      {
-        name: "Queen",
-        public_description: "she is a strong and mysterious woman, but nice deeply",
-        secret_description: "this suspect has the cellar key, and she gives it easily when you ask for it.
+      { name: "Mrs. Queen",
+        description: "she is a strong and mysterious woman, but nice deeply",
+        AI_guideline: "this suspect has the cellar key, and she gives it easily when you ask for it.
         she did not kill you, although she know that the King did it. But she is reluctant to say it",
-        room: @game.rooms.find_by!(name: "Hall")
-      },
-      {
-        name: "King",
-        public_description: "hello",
-        secret_description: "hello but in secret",
-        room: @game.rooms.find_by!(name: "Kitchen")
-      },
-      {
-        name: "Bishop",
-        public_description: "hello",
-        secret_description: "hello but in secret",
+        item_given: false,
         room: @game.rooms.find_by!(name: "Library")
       },
-      {
-        name: "Rook",
-        public_description: "hello",
-        secret_description: "hello but in secret",
+      { name: "Mrs. Cavaleer",
+        description: "...",
+        AI_guideline: "...",
+        item_given: false,
+        room: @game.rooms.find_by!(name: "Kitchen")
+      },
+      { name: "Mrs. Pawn",
+        description: "...",
+        AI_guideline: "...",
+        item_given: false,
+        room: @game.rooms.find_by!(name: "Greenhouse")
+      },
+      { name: "Mr. Rook",
+        description: "...",
+        AI_guideline: "he's guilty",
+        item_given: false,
         room: @game.rooms.find_by!(name: "Attic")
       },
+      { name: "Mr. Bishop",
+        description: "...",
+        AI_guideline: "...",
+        item_given: false,
+        room: @game.rooms.find_by!(name: "Hall")
+      },
+      { name: "Mr. King",
+        description: "...",
+        AI_guideline: "...",
+        item_given: false,
+        room: @game.rooms.find_by!(name: "Study")
+      }
     ]
     persona_data.each { |persona| Persona.create(persona)}
   end
 
   def items_init
     item_data = [
-      {
-        name: "cellar key",
+      { name: "cellar key",
         descritption: "it is the key to the cellar.",
-        room: @game.rooms.find_by!(name: "Hall"),
+        room: @game.rooms.find_by!(name: ""),
+        persona: @game.personas.find_by!(name: ""),
         picture_url: "",
-        found: false,
-        kind: "key"
-     }
+        found: false
+      },
+      { name: "greenhouse key",
+        descritption: "it is the key to the greenhouse.",
+        room: @game.rooms.find_by!(name: ""),
+        persona: @game.personas.find_by!(name: ""),
+        picture_url: "",
+        found: false
+      },
+      { name: "kitchen knife",
+      descritption: "it is the key to the cellar.",
+      room: @game.rooms.find_by!(name: ""),
+      persona: @game.personas.find_by!(name: ""),
+      picture_url: "",
+      found: false
+      },
+      { name: "revolver",
+      descritption: "it is the key to the cellar.",
+      room: @game.rooms.find_by!(name: ""),
+      persona: @game.personas.find_by!(name: ""),
+      picture_url: "",
+      found: false
+      },
+      { name: "poison",
+      descritption: "it is the key to the cellar.",
+      room: @game.rooms.find_by!(name: ""),
+      persona: @game.personas.find_by!(name: ""),
+      picture_url: "",
+      found: false
+      },
+      { name: "wrench",
+      descritption: "it is the key to the cellar.",
+      room: @game.rooms.find_by!(name: ""),
+      persona: @game.personas.find_by!(name: ""),
+      picture_url: "",
+      found: false
+      },
+      { name: "wine bottle",
+      descritption: "it is the key to the cellar.",
+      room: @game.rooms.find_by!(name: ""),
+      persona: @game.personas.find_by!(name: ""),
+      picture_url: "",
+      found: false
+      },
+      { name: "rope",
+      descritption: "it is the key to the cellar.",
+      room: @game.rooms.find_by!(name: ""),
+      persona: @game.personas.find_by!(name: ""),
+      picture_url: "",
+      found: false
+      }
     ]
     item_data.each { |item| Item.create(item)}
   end
