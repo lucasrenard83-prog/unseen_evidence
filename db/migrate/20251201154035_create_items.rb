7838da1300ffc1e096f78539604ff0f12451a680
saif harrag <saifharrag1@gmail.com>
2025-12-04 12:40:09 +0100

    navbar colorée

--- db/migrate/20251201154035_create_items.rb
+++ db/migrate/20251201154035_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
       t.string :name
-      t.string :descritption
+      t.string :description
       t.references :room, null: false, foreign_key: true
       t.string :picture_url
       t.boolean :found
--- db/migrate/20251203134112_edit_public_desc_and_secret_desc_from_persona.rb
+++ db/migrate/20251203134112_edit_public_desc_and_secret_desc_from_persona.rb
@@ -1,6 +1,6 @@
 class EditPublicDescAndSecretDescFromPersona < ActiveRecord::Migration[7.1]
   def change
-    rename_column :personas, :public_description, :descritption
+    rename_column :personas, :public_description, :description
     rename_column :personas, :secret_description, :ai_guideline
   end
 end
