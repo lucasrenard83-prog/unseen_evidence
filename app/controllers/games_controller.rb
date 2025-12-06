6d38fb6599833748f5a175c3a798b39dd643cf19
Mitia Blaise <git@github.com>
2025-12-06 22:06:53 +0100

    Front fix

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -46,13 +46,11 @@ class GamesController < ApplicationController
 
   def confront
     @game = Game.find(params[:id])
-
     killer_correct = params[:killer] == "Mr. Rook"
     room_correct = params[:room] == "Greenhouse"
     weapon_correct = params[:weapon] == "Kitchen knife"
 
     if killer_correct && room_correct && weapon_correct
-      redirect_to room_path(@game.rooms.first), notice: "Congratulations"
     else
       redirect_to room_path(@game.rooms.first), notice: "Wrong Accusation"
     end
